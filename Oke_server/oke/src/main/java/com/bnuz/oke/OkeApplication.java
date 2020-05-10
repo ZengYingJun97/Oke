72f739ba606302c7d6772356803175c6b1072676
zengyingjun <13692916769@163.com>
2020-05-10 14:16:55 +0800

    init server

---
+++ Oke_server/oke/src/main/java/com/bnuz/oke/OkeApplication.java
@@ -0,0 +1,13 @@
+package com.bnuz.oke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkeApplication.class, args);
+	}
+
+}
---
+++ Oke_server/oke/src/test/java/com/bnuz/oke/OkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnuz.oke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
