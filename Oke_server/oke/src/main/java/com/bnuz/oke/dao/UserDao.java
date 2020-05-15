fa4267c9c4eed550ba94712a3039122be1d5330a
zengyingjun <13692916769@163.com>
2020-05-15 23:35:44 +0800

    UserDao finish

---
+++ Oke_server/oke/src/main/java/com/bnuz/oke/dao/UserDao.java
@@ -0,0 +1,20 @@
+package com.bnuz.oke.dao;
+
+import com.bnuz.oke.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
+
+/**
+ * UserDao
+ *
+ * @author handsome
+ * @date 2020年 05月15日 23:17:02
+ */
+@Mapper
+@Component
+public interface UserDao {
+
+	User queryByUsername(String username);
+
+	void insertUser(User user);
+}
