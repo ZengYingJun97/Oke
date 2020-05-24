ed316caa40899a2472c8aefd42cdd23183345789
Administrator <Administrator@USER-GCT0CVIOTE>
2020-05-24 13:32:01 +0800

    11

--- Oke_client/Oke_teacher/Oke_teacher/OkeRibbon.Designer.cs
+++ Oke_client/Oke_teacher/Oke_teacher/OkeRibbon.Designer.cs
@@ -50,7 +50,7 @@
             // LoginGroup
             // 
             this.LoginGroup.Items.Add(this.LoginButton);
-            this.LoginGroup.Label = "登录";
+            this.LoginGroup.Label = "个人中心";
             this.LoginGroup.Name = "LoginGroup";
             // 
             // LoginButton
