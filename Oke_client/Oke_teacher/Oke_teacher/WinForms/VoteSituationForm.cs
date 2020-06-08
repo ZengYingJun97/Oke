828d47e6dc299a53e596012b4029064ad351fe00
hysan1112 <765820323@qq.com>
2020-06-09 01:05:50 +0800

    投票反馈 100%

--- Oke_client/Oke_teacher/Oke_teacher/WinForms/VoteSituationForm.cs
+++ Oke_client/Oke_teacher/Oke_teacher/WinForms/VoteSituationForm.cs
@@ -15,7 +15,7 @@ using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Documents;
+
 using System.Windows.Forms;
 
 namespace Oke_teacher.WinForms
